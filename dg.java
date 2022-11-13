a66696c40be3b221c7be74d65520f5e6f76ad02e
NMBVC <117529893+NastyaBaburova@users.noreply.github.com>
2022-11-13 19:29:28 +0300

    Add files via upload

---
+++ dg.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class dg {
+    public static void main(String[] agrs) {
+        for (int x = 50001; x<=100000; x+=2){
+
+            System.out.println(x);
+        }
+    }
+
+}
